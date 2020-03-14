2ec86369fdbdb057e087cd55ac684e2b8dc504e7
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-14 19:54:12 +0200

    Remove navigational properties.

--- src/boardcell.js
+++ src/boardcell.js
@@ -2,13 +2,9 @@ class BoardCell{
 
     owner;
     isFrozen;
-    leftSibling;
-    rightSibling;
 
     constructor(owner) {
-        this.owner = owner;
         this.isFrozen = false;
-        this.leftSibling = null;
-        this.rightSibling = null;
+        this.owner = owner;
     }
 }
