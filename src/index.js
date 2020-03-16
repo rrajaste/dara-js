a6001a6d24c076fd0481f003734e9bcf6da81a87
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-16 21:12:08 +0200

    Add test functionality to controller

--- src/index.js
+++ src/index.js
@@ -1,45 +1,94 @@
+import {GAME_PHASES} from "./gamephases.js";
+import {PLAYER_TYPES} from "./playertypes.js";
+
 "js-cell";
 
-//let gameEngine = new gameEngine();
+import GameEngine from "./gameengine.js";
+import Coordinate from "./coordinate.js";
 
-let board = document.getElementById('gameBoard');
-let rows = board.querySelectorAll('.js-board-row');
+let engine = new GameEngine();
+engine.startGame();
+
+console.log(engine);
+
+displayStats();
+setErrorBoxText("\n");
+
+let displayBoard = document.getElementById('gameBoard');
+let displayBoardRows = displayBoard.querySelectorAll('.js-board-row');
 
 let playerColorMap = {
-    '0': 'white',
-    '1': 'green',
-    '2': 'blue'
+    "First Player": "blue",
+    "Second Player": "green",
+    "Empty": "brown"
 };
 
+
+let cellToMove;
+
 function cellClicked(event) {
+    setErrorBoxText("\n");
+
     let row = this.dataset.row;
     let col = this.dataset.col;
+    let coordinate = new Coordinate(col, row);
 
-    // gameEngine.makeMove(col, row);
-    //
-    // let owner = gameEngine.getCellOwner(col, row);
-
-    let owner = this.dataset.player;
-
-    console.log(owner)
+    if (engine.gamePhase === GAME_PHASES.DROP_PHASE)
+    try {
+        engine.claimCell(coordinate);
+        setCellColor(this, playerColorMap[engine.whoseTurn.PLAYER_TYPE]);
+    } catch (e) {
+        setErrorBoxText(e.message);
+    } else if (engine.gamePhase === GAME_PHASES.MOVE_PHASE) {
+        if (cellToMove === undefined) {
+            cellToMove = this;
+        } else {
+            engine.moveToken()
+        }
+    }
+    displayStats();
+    console.log(engine);
 }
 
-function setDisplayCell(element, owner) {
-    clearCellColorClass(element);
-    setColorClass(element, playerColorMap[owner]);
+function setCellColor(element, color) {
+    clearCellColor(element);
+    element.classList.add(color)
 }
 
-function clearCellColorClass(element){
-    let owner = element.dataset.player;
-    let colorClass = playerColorMap[owner];
-    element.classList.remove(colorClass);
+function clearCellColor(element) {
+
+    for (const player in playerColorMap) {
+        let color = playerColorMap[player];
+        if (element.classList.contains(color)){
+            element.classList.remove(color);
+        }
+    }
 }
 
-function setColorClass(element, colorClass){
-    element.classList.add(colorClass);
+function setErrorBoxText(text){
+    document.getElementById("js-error-box").innerText = text;
 }
 
-for (const row of rows) {
+function displayTokenCount() {
+    document.getElementById("first-player-token-counter").innerText = engine.firstPlayer.tokenCount;
+    document.getElementById("second-player-token-counter").innerText = engine.secondPlayer.tokenCount;
+}
+
+function displayGamePhase() {
+    document.getElementById("game-phase").innerText = engine.gamePhase;
+}
+
+function displayWhoseTurnItIs() {
+    document.getElementById("whose-turn").innerText = engine.whoseTurn.toString();
+}
+
+function displayStats() {
+    displayTokenCount();
+    displayWhoseTurnItIs();
+    displayGamePhase()
+}
+
+for (const row of displayBoardRows) {
     let cells = row.querySelectorAll('.js-cell');
     for (const cell of cells)  {
         cell.onclick = cellClicked;
