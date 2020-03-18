ad803f0b35d86a15bd1b752f7239b8c0f11fa3a1
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-18 18:47:04 +0200

    Rewrite traverser class
    Rewrite class to be more generic and flexible

--- src/boardTraverser.js
+++ src/boardTraverser.js
@@ -1,21 +1,35 @@
+import Coordinate from "./coordinate.js";
+import Direction from "./direction.js";
+
 export default class BoardTraverser {
 
     constructor(board) {
         this.board = board;
-        this.startingCoordinate = undefined;
-        this.moveDirection = undefined;
     }
 
-    getCoordinatesOfFirstCellMatchingPredicate(predicate){
+    traverseRow(coordinate, output){
+        let startingCoordinate = new Coordinate(0, coordinate.y);
+        let moveDirection = new Direction(1, 0);
+        this.traverse(startingCoordinate, moveDirection, output);
+    }
 
-        let currentCoordinate = this.startingCoordinate;
+    traverseColumn(coordinate, output){
+        let startingCoordinate = new Coordinate(coordinate.x, 0);
+        let moveDirection = new Direction(0, 1);
+        this.traverse(startingCoordinate, moveDirection, output);
+    }
 
-        while (currentCoordinate.y < this.board.numberOfRows && currentCoordinate.x < this.board.numberOfColumns) {
+    traverse(startingCoordinate, moveDirection, output){
+
+        let currentCoordinate = startingCoordinate;
+        while (this._isCoordinateWithinBorders(currentCoordinate)) {
+
             let boardCell = this.board.getCell(currentCoordinate);
-            if (predicate(boardCell)) {
-                return currentCoordinate;
-            }
-            currentCoordinate = currentCoordinate.addDirection(this.moveDirection);
+            output(boardCell);
+            currentCoordinate = currentCoordinate.addDirection(moveDirection);
         }
+    }
+    _isCoordinateWithinBorders(currentCoordinate){
+        return currentCoordinate.y < this.board.numberOfRows && currentCoordinate.x < this.board.numberOfColumns;
     }
 }
