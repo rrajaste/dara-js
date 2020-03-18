e24b6136dd187bd1715380bc5c382357b94ec427
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-18 20:30:54 +0200

    Separate player's total token count from available token count

--- src/player.js
+++ src/player.js
@@ -1,6 +1,8 @@
 export default class {
+
     constructor(PLAYER_TYPE) {
-        this.tokenCount = 12;
+        this.totalTokenCount = 12;
+        this.unusedTokenCount = 12;
         this.PLAYER_TYPE = PLAYER_TYPE;
         this.Name;
     }
