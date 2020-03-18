20038b1d7f0cdf34dc08085914bcdc388c61984b
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-18 15:40:19 +0200

    Add enum for semantic directions

--- src/direction.js
+++ src/direction.js
@@ -1,3 +1,5 @@
+import {DIRECTIONS} from "./directions.js";
+
 export default class {
     constructor(rowIncrement, columnIncrement) {
         this.rowIncrement = this._normalizeIncrement(rowIncrement);
@@ -9,6 +11,7 @@ export default class {
         let columnIncrement = destinationCoordinate.x - startingCoordinate.x;
         return new this(rowIncrement, columnIncrement);
     }
+
     _normalizeIncrement(increment){
         if (increment > 1) {
             increment = 1;
@@ -16,5 +19,29 @@ export default class {
             increment = -1
         }
         return increment;
+    }
+
+    getSemanticDirection(){
+        if (this.x === 0 && this.y === -1) {
+            return DIRECTIONS.NORTH;
+        } else if (this.x === 0 && this.y === 1){
+            return DIRECTIONS.SOUTH;
+        } else if (this.x === 1 && this.y === 0){
+            return DIRECTIONS.EAST;
+        } else if (this.x === -1 && this.y === 0){
+            return DIRECTIONS.WEST;
+        } else if (this.x === 1 && this.y === -1){
+            return DIRECTIONS.NORTH_EAST;
+        } else if (this.x === -1 && this.y === -1){
+            return DIRECTIONS.NORTH_WEST
+        } else if (this.x === 1 && this.y === 1){
+            return DIRECTIONS.SOUTH_EAST
+        } else if (this.x === -1 && this.y === 1){
+            return DIRECTIONS.SOUTH_WEST
+        }
+    }
+
+    toString(){
+        return this.getSemanticDirection();
     }
 }
