8df89eedbf7612412d1a134c73a2b98c4c0c39fd
RannoRajaste <ranno.rajaste@gmail.com>
2020-03-26 23:03:30 +0200

    Set token count back to 12

--- src/player.js
+++ src/player.js
@@ -1,8 +1,8 @@
 export default class {
 
     constructor(PLAYER_TYPE) {
-        this.totalTokenCount = 7;
-        this.unusedTokenCount = 7;
+        this.totalTokenCount = 12;
+        this.unusedTokenCount = 12;
         this.PLAYER_TYPE = PLAYER_TYPE;
         this.Name;
     }
